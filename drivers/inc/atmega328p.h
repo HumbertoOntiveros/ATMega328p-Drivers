5f8a25c8e1e407de03183e80761f1b75423d08b5
HumbertoOntiveros <humbertoontiveros08@gmail.com>
2024-10-27 17:28:37 -0600

    Refactor SPI bit position definitions and ISR macro in atmega328p.h
    - Refactored SPI bit position definitions by prefixing SPI-related macros with SPI_ for enhanced clarity and consistency:
        - SPSR_SPI2X, SPSR_WCOL, and SPSR_SPIF updated to SPI_SPSR_SPI2X, SPI_SPSR_WCOL, and SPI_SPSR_SPIF.
        - SPCR_* macros updated to SPI_SPCR_* format for improved readability.

--- drivers/inc/atmega328p.h
+++ drivers/inc/atmega328p.h
@@ -242,21 +242,21 @@ typedef struct
 /*
  * Bit position definitions for SPI SPSR REG
  */
-#define SPSR_SPI2X     0
-#define SPSR_WCOL      6
-#define SPSR_SPIF      7
+#define SPI_SPSR_SPI2X     0
+#define SPI_SPSR_WCOL      6
+#define SPI_SPSR_SPIF      7
 
 /*
  * Bit position definitions for SPI SPCR REG
  */
- #define SPCR_SPR0     0
- #define SPCR_SPR1     1
- #define SPCR_CPHA     2
- #define SPCR_CPOL     3
- #define SPCR_MSTR     4
- #define SPCR_DORD     5
- #define SPCR_SPE      6
- #define SPCR_SPIE     7
+ #define SPI_SPCR_SPR0     0
+ #define SPI_SPCR_SPR1     1
+ #define SPI_SPCR_CPHA     2
+ #define SPI_SPCR_CPOL     3
+ #define SPI_SPCR_MSTR     4
+ #define SPI_SPCR_DORD     5
+ #define SPI_SPCR_SPE      6
+ #define SPI_SPCR_SPIE     7
 
 /*
  * Generic Macros Definition
