bf30136f88eaf80b1d87f0068ffd55fa64e5a021
HumbertoOntiveros <humbertoontiveros08@gmail.com>
2024-12-26 15:37:50 -0700

    Refactor: Optimize LCD pin macros
    - Updated pin definitions to use shorter macros (PIN0, PIN1, etc.)

--- bsp/lcd.h
+++ bsp/lcd.h
@@ -23,13 +23,13 @@
  * Hardware connections and pin definitions for LCD interface 
  */
 #define LCD_GPIO_PORT  GPIOD
-#define LCD_GPIO_RS	   GPIO_PIN_NO_0
-#define LCD_GPIO_RW	   GPIO_PIN_NO_1
-#define LCD_GPIO_EN	   GPIO_PIN_NO_2
-#define LCD_GPIO_D4	   GPIO_PIN_NO_3
-#define LCD_GPIO_D5	   GPIO_PIN_NO_4
-#define LCD_GPIO_D6	   GPIO_PIN_NO_5
-#define LCD_GPIO_D7	   GPIO_PIN_NO_6
+#define LCD_GPIO_RS	   PIN0
+#define LCD_GPIO_RW	   PIN1
+#define LCD_GPIO_EN	   PIN2
+#define LCD_GPIO_D4	   PIN3
+#define LCD_GPIO_D5	   PIN4
+#define LCD_GPIO_D6	   PIN5
+#define LCD_GPIO_D7	   PIN6
 
 /*
  * LCD Control Commands
