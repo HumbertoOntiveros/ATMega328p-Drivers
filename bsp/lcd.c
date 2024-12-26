42d8b76a8b2c516fdf2e859ef14cacf42145a19c
HumbertoOntiveros <humbertoontiveros08@gmail.com>
2024-12-26 11:33:51 -0700

    Add LCD driver source and header files to project
    - Added lcd.c and lcd.h for the LCD driver functionality.
    - Included lcd.o in the object files list for proper compilation.
    - Updated the Makefile to integrate the LCD driver into the build process.

---
+++ bsp/lcd.c
@@ -0,0 +1,57 @@
+/*
+ * @file              lcd.c
+ *
+ * @brief             XXXXX driver implementation for ATmega328P microcontroller.
+ *
+ * @details           This file provides the implementation of functions to initialize, 
+ *                    de-initialize, etc, etc, XXXX for the ATmega328P.
+ *                    It defines all the APIs to manage XXXX peripherals including
+ *                    initialization, data handling, and XXXXX operations.
+ *
+ * @author            JESUS HUMBERTO ONTIVEROS MAYORQUIN
+ * @date              26/12/2024 11:24:35
+ *
+ * @note              This driver is specifically designed for the ATmega328P microcontroller.
+ *                    Ensure proper configurations before using these functions.
+ */
+
+#include "lcd.h"
+
+/*********************************************************************
+ * @fn            - 
+ *
+ * @brief             - 
+ *
+ * @param[in]         - 
+ *
+ * @return            - 
+ *
+ * @Note              -
+ */
+
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 HumbertoOntiveros
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Happy coding!
+ */
