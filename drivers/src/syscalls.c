6fd8df0345744f253a2733f1cf66b24478c46691
HumbertoOntiveros <humbertoontiveros08@gmail.com>
2024-12-30 15:40:58 -0700

    Add UART stdin initialization and reading functionality to syscalls
    - Added UART_Init_Stdin function to initialize UART for receiving data (stdin).
    - Implemented _read function to read data from UART (mapped to stdin).
    - Declared uart_stdin FILE structure for directing stdin to UART.
    - Changed return type of _write function to int for compliance.

--- drivers/src/syscalls.c
+++ drivers/src/syscalls.c
@@ -12,6 +12,14 @@ void UART_Init(unsigned int ubrr)
     UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
 
+void UART_Init_Stdin(unsigned int ubrr)
+{
+    UBRR0H = (unsigned char)(ubrr >> 8);
+    UBRR0L = (unsigned char)ubrr;
+    UCSR0B = (1 << RXEN0);
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
 void UART_Transmit(char data)
 {
     while (!(UCSR0A & (_BV(UDRE0))) )
@@ -19,7 +27,7 @@ void UART_Transmit(char data)
     UDR0 = data;
 }
 
-ssize_t _write(char var, FILE *stream)
+int _write(char var, FILE *stream)
 {
     // translate \n to \r for br@y++ terminal
     //if (var == '\n') usart_putchar('\r');
@@ -28,5 +36,14 @@ ssize_t _write(char var, FILE *stream)
     
 }
 
+// Función de lectura para stdin (UART)
+int _read(FILE *stream)
+{
+    while (!(UCSR0A & (1 << RXC0)))  // Espera hasta que haya datos disponibles para recibir
+        ;
+    return (UDR0);  // Retorna el dato recibido
+}
+
 // Custom FILE structure to direct stdout to UART
 FILE uart_stdout = FDEV_SETUP_STREAM(_write, NULL, _FDEV_SETUP_WRITE);
+FILE uart_stdin = FDEV_SETUP_STREAM(NULL, _read, _FDEV_SETUP_READ);
